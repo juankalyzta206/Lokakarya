6d287169632d89003dee380c5c781e3be09cdf8b
Irzan Maulana <maulanairzan5@gmaiil.com>
2023-01-11 13:33:24 +0700

    sad

---
+++ src/main/java/com/ogya/lokakarya/util/PagingRequestWrapper.java
@@ -0,0 +1,62 @@
+package com.ogya.lokakarya.util;
+
+import java.util.List;
+
+public class PagingRequestWrapper {
+
+private Integer offSet;
+private Integer page;
+private Integer size;
+private String sortField;
+private String sortOrder;
+private List<FilterWrapper> filters = null;
+
+public Integer getOffSet() {
+return offSet;
+}
+
+public void setOffSet(Integer offSet) {
+this.offSet = offSet;
+}
+
+public Integer getPage() {
+return page;
+}
+
+public void setPage(Integer page) {
+this.page = page;
+}
+
+public Integer getSize() {
+return size;
+}
+
+public void setSize(Integer size) {
+this.size = size;
+}
+
+public String getSortField() {
+return sortField;
+}
+
+public void setSortField(String sortField) {
+this.sortField = sortField;
+}
+
+public String getSortOrder() {
+return sortOrder;
+}
+
+public void setSortOrder(String sortOrder) {
+this.sortOrder = sortOrder;
+}
+
+public List<FilterWrapper> getFilters() {
+return filters;
+}
+
+public void setFilters(List<FilterWrapper> filters) {
+this.filters = filters;
+}
+
+}
