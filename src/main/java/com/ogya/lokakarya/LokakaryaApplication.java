6b6926ecf7d3ebf340548b7bbcf214419f4bb698
Irzan Maulana <maulanairzan5@gmaiil.com>
2023-01-16 16:08:50 +0700

    command line runner sama override

--- src/main/java/com/ogya/lokakarya/LokakaryaApplication.java
+++ src/main/java/com/ogya/lokakarya/LokakaryaApplication.java
@@ -1,13 +1,19 @@
 package com.ogya.lokakarya;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LokakaryaApplication {
+public class LokakaryaApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LokakaryaApplication.class, args);
+	}
+	
+	@Override
+	public void run(String...args) throws Exception{
+	
 	}
 
 }
