e4977b6c31445664c18128637785918ad8a4f909
Juan Kalyzta <juankalyzta206@gmail.com>
2023-01-17 11:37:04 +0700

    .

---
+++ src/main/java/com/ogya/lokakarya/util/DataResponseFeign.java
@@ -0,0 +1,78 @@
+package com.ogya.lokakarya.util;
+
+public class DataResponseFeign<T> {
+	private boolean status;
+	private String message;
+	private Long timeStamp;
+	private T data;
+	private Boolean success;
+	private String referenceNumber;
+
+	public DataResponseFeign() {
+		super();
+	}
+
+	public DataResponseFeign(T data) {
+		this(true, null, data);
+	}
+
+	public DataResponseFeign(boolean status, String message) {
+		this(status, message, null);
+	}
+
+	public DataResponseFeign(boolean status, String message, T data) {
+		this.timeStamp = System.currentTimeMillis();
+		this.status = status;
+		this.message = message;
+		this.data = data;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public Long getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(Long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public String getReferenceNumber() {
+		return referenceNumber;
+	}
+
+	public void setReferenceNumber(String referenceNumber) {
+		this.referenceNumber = referenceNumber;
+	}
+
+}
