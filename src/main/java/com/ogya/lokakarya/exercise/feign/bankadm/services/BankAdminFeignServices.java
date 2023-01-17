034ddcfdfacb4c6824a88daa9c66b7f1019c309a
Juan Kalyzta <juankalyzta206@gmail.com>
2023-01-17 10:03:34 +0700

    .

--- src/main/java/com/ogya/lokakarya/exercise/feign/bankadm/services/BankAdminFeignServices.java
+++ src/main/java/com/ogya/lokakarya/exercise/feign/bankadm/services/BankAdminFeignServices.java
@@ -1,11 +1,12 @@
 package com.ogya.lokakarya.exercise.feign.bankadm.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ogya.lokakarya.exercise.feign.bankadm.repository.BankAdminFeignRepository;
 import com.ogya.lokakarya.exercise.feign.bankadm.request.BankAdminFeignRequest;
 import com.ogya.lokakarya.exercise.feign.bankadm.response.BankAdminFeignResponse;
-
+@Service
 public class BankAdminFeignServices {
 	@Autowired
 	private BankAdminFeignRepository bankAdminFeignRepository;
