d38bf64b4ef2a00a2abb9b6f7f272dd89c416805
Syihanghifari <syihanghifari27@gmail.com>
2023-01-18 10:54:40 +0700

    try cors

---
+++ src/main/java/com/ogya/lokakarya/LokakaryaConfiguration.java
@@ -0,0 +1,17 @@
+package com.ogya.lokakarya;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+public class LokakaryaConfiguration extends WebMvcConfigurerAdapter{
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
