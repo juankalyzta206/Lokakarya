f7243eab749423ec12710d53275fb2d50e7cc96e
rizkiauliasa <rizkiauliasa@student.uns.ac.id>
2023-01-20 07:48:19 +0700

    Notifikasi Setor

---
+++ src/main/java/com/ogya/lokakarya/exercise/feign/nasabah/controller/LaporanHistorySetorHarianController.java
@@ -0,0 +1,48 @@
+package com.ogya.lokakarya.exercise.feign.nasabah.controller;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itextpdf.text.DocumentException;
+import com.ogya.lokakarya.bankadm.notification.SetorNotification;
+
+@RestController
+@RequestMapping(value = "/laporanHistorySetor")
+@CrossOrigin(origins = "*")
+public class LaporanHistorySetorHarianController {
+	@Autowired
+	SetorNotification setor;
+	
+//	@RequestMapping(value = "/laporanSetor", method = RequestMethod.GET)
+//    public void notifSetorHarian() throws MessagingException, IOException, DocumentException {
+//		setor.historyNotification();
+//	}
+	@RequestMapping(value = "/laporanSetorDaily", method = RequestMethod.GET)
+    public void notifSetorDaily() throws MessagingException, IOException, DocumentException {
+		setor.historyNotificationDaily();
+	}
+	
+	@RequestMapping(value = "/laporanSetorWeekly", method = RequestMethod.GET)
+    public void notifSetorWeekly() throws MessagingException, IOException, DocumentException {
+		setor.historyNotificationWeekly();
+	}
+	
+	@RequestMapping(value = "/laporanSetorMonthly", method = RequestMethod.GET)
+    public void notifSetorMonthly() throws MessagingException, IOException, DocumentException {
+		setor.historyNotificationMonthly();
+	}
+	
+//	@RequestMapping(value = "/exportToPdfALLSetor", method = RequestMethod.GET)
+//    public void exportToPdfsetor() throws Exception {
+//        setor.historyNotification();
+//      }
+}
